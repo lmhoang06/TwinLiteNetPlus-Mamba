f5de3570f9db90e907b7fe10863a2ab42038e6aa
Lê Minh Hoàng <minhhoanghsftg@gmail.com>
2025-10-27 01:20:46 +0000

    change to other to put to VM

--- train.py
+++ train.py
@@ -8,7 +8,7 @@ from copy import deepcopy
 from argparse import ArgumentParser
 import csv
 
-from model.model import TwinLiteNetPlus, TwinLiteNetPlus_V3, TwinLiteNetPlus_Vmamba2
+from model.model import TwinLiteNetPlus, TwinLiteNetPlus_V3, TwinLiteNetPlus_Vmamba, TwinLiteNetPlus_Lightweight
 from loss import TotalLoss
 from utils import train, val, netParams, save_checkpoint, poly_lr_scheduler
 import BDD100K
@@ -39,7 +39,7 @@ def train_net(args, hyp):
     cuda_available = torch.cuda.is_available()
     num_gpus = torch.cuda.device_count()
     
-    model = TwinLiteNetPlus_Vmamba2(args)
+    model = TwinLiteNetPlus_Lightweight(args)
     # pretrained_dict = torch.load('./pretrained/small.pth')
     # model_dict = model.state_dict()
     # # Only keep keys that exist in both model and pretrained weights
